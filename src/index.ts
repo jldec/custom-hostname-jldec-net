a46f72eaf38894e2f2ed42a8dc347a1d71a15df2
jldec <jurgen@jldec.me>
2024-11-25 17:16:42 +0000

    cleaner

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export default {
   async fetch(request, env, ctx): Promise<Response> {
     const headers = Object.fromEntries(request.headers.entries())
     return Response.json(headers)
-  },
+  }
 } satisfies ExportedHandler<Env>
