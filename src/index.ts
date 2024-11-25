169180e0f20a1da4be94b9c071cd0af496d72477
jldec <jurgen@jldec.me>
2024-11-25 19:11:26 +0000

    expand response

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 export default {
   async fetch(request, env, ctx): Promise<Response> {
     const headers = Object.fromEntries(request.headers.entries())
-    return Response.json(headers)
+    return Response.json({
+      zone: 'jldec.net',
+      url: request.url,
+      time: new Date().toISOString(),
+      headers,
+      cf: request.cf
+    })
   }
 } satisfies ExportedHandler<Env>
